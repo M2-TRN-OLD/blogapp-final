cab6bdf0ad25ca00f78d1dd49b6352c4821495e7
Missy Maloney <missymaloney1@gmail.com>
2018-12-29 18:28:50 -0500

    added mongodb on mLab database

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 'use strict';
 
 exports.DATABASE_URL = 
-  process.env.DATABASE_URL || 'mongodb://localhost/blogapp-finaldb';
+  //process.env.DATABASE_URL || 'mongodb://localhost/blogDb';
+  process.env.DATABASE_URL || 'mongodb://admin:Daisl9515@ds151955.mlab.com:51955/blogapp-finaldb1'
+
 exports.PORT = process.env.PORT || 8080;
